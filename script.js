57baabf845cb40a76e5fe428fd4ab3b31aa993a4
Francisco Medina <medina.juanfrancisco@gmail.com>
2019-10-22 22:48:35 -0500

    COmmit

--- script.js
+++ script.js
@@ -2,25 +2,24 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+
 function DoneUndone() {
 	let li = document.querySelectorAll("li");
 	for (let i = 0; i < li.length; i++) {
-		const element = li[i];
+		let element = li[i];
 		element.addEventListener('click', function () {
-			if (element.className != "done") element.classList.add("done");
-			else element.classList.toggle("done");
+			if (element.classList != "done") element.classList.toggle("done");
+			else element.classList.toggle("done")
 		})
 	}
 }
 
-function Erasetask(){
-	let myList = document.getElementsByTagName("ul");
+function Erasetask() {
 	let button = document.querySelectorAll("button.erase");
-	let length = document.getElementsByClassName("erase");
-	for (let i = 0; i < length; i++) {
-		//const button = eraseButton[i];
-		button.addEventListener('click', function () {
-			myList.removeChild(myList.children[i]);
+	for (let i = 0; i < button.length; i++) {
+		let element = button[i];
+		element.addEventListener('click', function () {
+			element.parentElement.remove();
 		})
 	}
 }
@@ -30,34 +29,40 @@ function inputLength() {
 }
 
 function createListElement() {
-	DoneUndone();
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-	DoneUndone();
-	CreateEraseButton();
-	Erasetask();
-}
-
-function CreateEraseButton(){
 	let button = document.createElement("button");
+	li.appendChild(document.createTextNode(input.value));
 	button.appendChild(document.createTextNode("Remove"));
 	button.classList.add("erase");
-	ul.appendChild(button);
+	ul.appendChild(li);
+	li.appendChild(button);
+	input.value = "";
+
 }
+
+// function CreateEraseButton() {
+// 	let button = document.createElement("button");
+// 	button.appendChild(document.createTextNode("Remove"));
+// 	button.classList.add("erase");
+// 	ul.appendChild(button);
+// }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
+		DoneUndone();
+		Erasetask();
 	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
+		DoneUndone();
+		Erasetask();
 	}
 }
+
 
 DoneUndone();
 Erasetask();
